c26bbedc0930169d64ab2c28f879ccff71fc40a9
Joan-Robles <jroblesf@unal.edu.co>
2025-05-06 13:51:29 -0500

    First plots

---
+++ best_response.R
@@ -0,0 +1,21 @@
+# Title: Game Theory Simulation
+# Parameters --------------------------------------------------------------
+n_players <- seq(2, 10, 2) # Number of players in the game
+n_games <- 1e4 # Simulations per player count
+set.seed(1928) # For reproducibility
+
+# Call functions 
+source("functions.R")
+
+# Save the plots in a folder called plots
+
+if(!dir.exists("plots")) dir.create("plots")
+for(i in n_players) {
+  cat("Running game with", i, "players...\n")|
+  png(
+    filename = sprintf("plots/game_results_%d.png", i),
+    width    = 10, height = 6, units = "in", res = 300
+  )
+  summary_game_results(run_many_games(n_games, i), n_players = i)
+  dev.off()
+}
